8c4838828f084916b99578839d467f62553f773c
Shivam Saxena <43326734+shivsaxena91@users.noreply.github.com>
2023-01-23 14:43:01 -0800

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,14 @@
+def buildApp(){
+	echo 'Building the application.....'
+}
+
+def testApp(){
+	echo 'Testing the application....'
+}
+
+def deployApp(){
+	echo 'Deploying the application....'
+	echo "Deploying version ${params.VERSION}"
+}
+
+return this
